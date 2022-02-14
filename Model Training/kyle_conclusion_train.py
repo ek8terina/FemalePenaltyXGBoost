ffe360803cc4daeef160f33864d3b3ca75f99728
Ekaterina Fedorova <ek8terinafed@gmail.com>
2022-02-14 13:48:53 -0600

    training scripts

--- Model Training/kyle_conclusion_train.py
+++ Model Training/kyle_conclusion_train.py
@@ -4,16 +4,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 from training import rmse_how_training_went, test_predictions
+from statistics import mean
 
-
-data = pd.read_csv("../Data/train_test_data/abstracts_kyle.csv")
+data = pd.read_csv("../Data/train_test_data/conclusions_kyle.csv")
+data = data.drop(columns=['title'])
 indices = data['ArticleID']  # ArticleID
+count = 0
+for key in data.keys():
+    print(str(count) + ": " + key)
+    count = count + 1
 features = data.iloc[:, 1:390]  # see note above for list (note: 390 is not included as per split obj format)
-regressand = data[["ArticleID", "hedge_abstract_change"]]  # keep ArticleID just for ease
+regressand = data[["ArticleID", "hedge_conclusion_change"]]  # keep ArticleID just for ease
 
 # set seed for reproducability
 seed = 42  # change for experimentation
-test_split = .2  # data size = 3250
+test_split = .2  # full data size = ..
 
 # split
 x_train, x_test, y_train, y_test = train_test_split(features, regressand, test_size=test_split, random_state=seed)
@@ -26,7 +31,7 @@ model_2 = XGBRegressor()
 model_2.fit(x_train.iloc[:, 1:], y_train.iloc[:, 1], eval_metric="rmse", eval_set=test, verbose=True)
 
 # show how training went
-save_fig = "../Figures/kyle_abstract/model_selection.png"
+save_fig = "../Figures/kyle_conclusion/model_selection.png"
 rmse_how_training_went(model_2, save_file=save_fig)
 
 # look at prediction for test set
@@ -34,15 +39,41 @@ y_pred = model_2.predict(x_test.iloc[:, 1:])
 mse = mean_squared_error(list(y_test.iloc[:, 1]), list(y_pred))
 print("Here is MSE of this XGBoost model: " + str(mse))
 test = {'ArticleID': y_test.iloc[:, 0], 'y_test': y_test.iloc[:, 1], 'prediction': y_pred}
+test['avg_y_test'] = mean(y_test.iloc[:, 1])
 difference = np.subtract(np.array(list(y_pred)), np.array(list(y_test.iloc[:, 1])))
 sq_diff = [x**2 for x in list(difference)]
 test['error'] = difference
 test['squared_error'] = sq_diff
+# quick classification
+results_as_classifier = []
+accuracy = []
+for idx in range(len(test['y_test'])):
+    if test['prediction'][idx] == 0:
+        res = 0
+        results_as_classifier.append(0)
+    elif test['prediction'][idx] > 0:
+        res = 1
+        results_as_classifier.append(1)
+    else:
+        res = -1
+        results_as_classifier.append(-1)
+    if (res == 0) & (y_test.iloc[idx, 1] == 0):
+        accuracy.append(1)
+    elif (res == 1) & (y_test.iloc[idx, 1] > 0):
+        accuracy.append(1)
+    elif (res == -1) & (y_test.iloc[idx, 1] < 0):
+        accuracy.append(1)
+    else:
+        accuracy.append(0)
+test['results_as_classifier'] = results_as_classifier
+test['rough_accuracy'] = accuracy
+print("final accuracy if converting to classifer:")
+print(str(sum(accuracy)/len(accuracy)))
 test = pd.DataFrame.from_dict(test).sort_values(by=['squared_error'], ascending=False)
-test.to_csv("../Results/kyle_abstract_test.csv")
+test.to_csv("../Results/kyle_conclusion_test.csv")
 # save model
-model_2.save_model("../Models/kyle_abstract.txt")       # save as .txt, smaller than .json format
+model_2.save_model("../Models/kyle_conclusion.txt")       # save as .txt, smaller than .json format
 
 # create graph of test predictions
 x_ax = range(len(y_test.iloc[:, 1]))
-test_predictions(x_ax, y_test=y_test.iloc[:, 1], y_pred=y_pred, save_file="../Figures/kyle_abstract/test_results.png")
+test_predictions(x_ax, y_test=y_test.iloc[:, 1], y_pred=y_pred, save_file="../Figures/kyle_conclusion/test_results.png")
--- Model Training/training.py
+++ Model Training/training.py
@@ -1,54 +1,102 @@
-from numpy import loadtxt
-from xgboost import XGBClassifier
+import numpy as np
+from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from matplotlib import pyplot
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def rmse_how_training_went(model, save_file=None, ylab="RMSE", xlab="Epochs", title="XGBoost RMSE"):
+    training_results = model.evals_result()
+    epochs = len(training_results['validation_0']['rmse'])
+    x_axis = range(0, epochs)
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, training_results['validation_0']['rmse'], label='Train')
+    ax.plot(x_axis, training_results['validation_1']['rmse'], label='Test')
+    ax.legend()
+    plt.ylabel(ylab)
+    plt.xlabel(xlab)
+    plt.title(title)
+    if save_file is not None:
+        plt.savefig(save_file)
+    # plt.show()
+    plt.clf()
+
+def test_predictions(x_ax, y_test, y_pred, save_file, title = "Abstract hedge change test and predicted data"):
+    plt.plot(x_ax, y_test, label="original")
+    plt.plot(x_ax, y_pred, label="predicted")
+    plt.title(title)
+    plt.legend()
+    # plt.show()
+    plt.savefig(save_file)
+
 
 '''
 Load data
-features: BERT embeddings, gender
+features: SBERT embeddings, gender, publicationYear, published word number, original word number
 regressand: Change in hedge words
-split into test and training
+for print of keys, add this: 
+count = 0
+for key in data.keys():
+   print(str(count) + ": " + key)
+   count = count + 1
+stop
 
-'''
+data = pd.read_csv("../Data/train_test_data/abstracts_kyle.csv")
+indices = data['ArticleID']  # ArticleID
+features = data.iloc[:, 1:390]  # see note above for list (note: 390 is not included as per split obj format)
+regressand = data[["ArticleID", "hedge_abstract_change"]]  # keep ArticleID just for ease
+
 # set seed for reproducability
-seed = 42
-test_split = .2
+seed = 42  # change for experimentation
+test_split = .2  # data size = 3250
 
-'''
-basic code
-# split and train
-x_train, x_test, y_train, y_test = train_test_split(features, regressand, test_size=test_size, random_state=seed)
-basic = XGBRegressor()
-basic.fit(x_train, y_train)
-y_pred = basic.predict(x_test)
-
-# get accuracy
-accuracy = accuracy_score(y_test, predictions)
-print("Here is accuracy of most basic XGBoost model: " + accuracy)
-
-# save model
-basic.save_model("../Models/basic_model.txt")       # save as .txt, smaller than .json format
-
+# split
+x_train, x_test, y_train, y_test = train_test_split(features, regressand, test_size=test_split, random_state=seed)
 # load model
-# basic_loaded = xgb.XGBRegressor()
-# basic_loaded.load_model("../Models/basic_model.txt")
-'''
-
-'''
-slightly more in-depth
-test = [(x_train, y_train), (x_test, y_test)]
+loaded = XGBRegressor()
+loaded.load_model("../Models/basic_model.txt")
+# create train and validation set
+test = [(x_train.iloc[:, 1:], y_train.iloc[:, 1]), (x_test.iloc[:, 1:], y_test.iloc[:, 1])]
+# train XGBRegessor
 model_2 = XGBRegressor()
-# add early_stopping_rounds=z param
-model_2.fit(x_train, y_train, eval_metric="rmse", eval_set=test, verbose=True)      #print how model is doing
+# optional: add early_stopping_rounds=z param
+# verbose: print how model is doing
+model_2.fit(x_train.iloc[:, 1:], y_train.iloc[:, 1], eval_metric="rmse", eval_set=test, verbose=True)
 
+# show how training went
 training_results = model_2.evals_result()
-fig, ax = pyplot.subplots()
+epochs = len(training_results['validation_0']['rmse'])
+x_axis = range(0, epochs)
+fig, ax = plt.subplots()
 ax.plot(x_axis, training_results['validation_0']['rmse'], label='Train')
 ax.plot(x_axis, training_results['validation_1']['rmse'], label='Test')
 ax.legend()
-pyplot.ylabel('RMSE')
-pyplot.title('XGBoost RMSE')
-pyplot.show()
-'''
+plt.ylabel('RMSE')
+plt.ylabel('Epochs')
+plt.title('XGBoost RMSE')
+plt.savefig("../Figures/kyle_abstract/model_selection.png")
+# plt.show()
+plt.clf()
 
+y_pred = model_2.predict(x_test.iloc[:, 1:])
+mse = mean_squared_error(list(y_test.iloc[:, 1]), list(y_pred))
+print("Here is MSE of this XGBoost model: " + str(mse))
+test = {'ArticleID': y_test.iloc[:, 0], 'y_test': y_test.iloc[:, 1], 'prediction': y_pred}
+difference = np.subtract(np.array(list(y_pred)), np.array(list(y_test.iloc[:, 1])))
+sq_diff = [x**2 for x in list(difference)]
+test['error'] = difference
+test['squared_error'] = sq_diff
+test = pd.DataFrame.from_dict(test).sort_values(by=['squared_error'], ascending=False)
+test.to_csv("/Users/efedorov/Downloads/basic_model_mse.csv")
+# save model
+model_2.save_model("../Models/kyle_abstract.txt")       # save as .txt, smaller than .json format
+
+x_ax = range(len(y_test.iloc[:, 1]))
+plt.plot(x_ax, y_test.iloc[:, 1], label="original")
+plt.plot(x_ax, y_pred, label="predicted")
+plt.title("Abstract hedge change test and predicted data")
+plt.legend()
+# plt.show()
+plt.savefig("../Figures/kyle_abstract/test_results.png")
+'''
